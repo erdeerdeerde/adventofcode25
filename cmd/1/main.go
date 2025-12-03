75a1cf3928d2414aff7fe10781597df886d2b26c
Martin Weber <martin.weber@virtualminds.com>
2025-12-03 10:24:50 +0100

    better naming

--- cmd/1/main.go
+++ cmd/1/main.go
@@ -8,35 +8,35 @@ import (
 )
 
 func main() {
-	fi, err := os.Open("cmd/1/input_real.txt")
+	input_file, err := os.Open("cmd/1/input_real.txt")
 	if err != nil {
 		panic(err)
 	}
-	defer fi.Close()
-	scanner := bufio.NewScanner(fi)
-	d := 50
-	t := 0
+	defer input_file.Close()
+	scanner := bufio.NewScanner(input_file)
+	dial := 50
+	times := 0
 	for scanner.Scan() {
-		l := scanner.Text()
-		i, err := strconv.Atoi(l[1:])
+		line := scanner.Text()
+		num, err := strconv.Atoi(line[1:])
 		if err != nil {
 			panic(err)
 		}
-		if l[0] == 'R' {
-			d = (d + i) % 100
-			if d > 100 {
-				d = d - 100
+		if line[0] == 'R' {
+			dial = (dial + num) % 100
+			if dial > 100 {
+				dial = dial - 100
 			}
 		} else {
-			d = (d - i) % 100
-			if d < 0 {
-				d = d + 100
+			dial = (dial - num) % 100
+			if dial < 0 {
+				dial = dial + 100
 			}
 		}
-		fmt.Printf("%d\n", d)
-		if d == 0 {
-			t = t + 1
+		fmt.Printf("%dial\n", dial)
+		if dial == 0 {
+			times = times + 1
 		}
 	}
-	fmt.Printf("password: %d\n", t)
+	fmt.Printf("password: %d\n", times)
 }
